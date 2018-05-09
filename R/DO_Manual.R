7263a6ef13e2111cc10d5fb8af83ad7e822381f3
Paul R. Hibbing <paulhibbing@gmail.com>
2018-05-09 01:03:55 -0400

    General setup of generic-like structure

---
+++ R/DO_Manual.R
@@ -0,0 +1,10 @@
+#' Manually Collect Direct Observation Data
+#'
+#'
+#' @keywords internal
+#'
+#' @examples
+#' data_collection_program_manual()
+data_collection_program_manual <- function() {
+  return(NULL)
+}
---
+++ R/Pseudo_Generics.R
@@ -0,0 +1,39 @@
+#' Collect Direct Observation Data
+#'
+#' A generic-like function for collecting direct observation data manually or
+#' interactively, depending on user-specified preference and session
+#' capabilities.
+#'
+#' @param interactive Logical. Should program be run interactively?
+#'
+#' @export
+#' @note If `interactive = TRUE`, but R is not being used interactively, a
+#'   message is issued and an override is implemented to set `interactive =
+#'   FALSE`.
+#'
+#' @seealso [data_collection_program_interactive()],
+#'     [data_collection_program_manual()]
+#'
+#' @examples
+#' if (interactive()) {
+#' data_collection_program()
+#' }
+#'
+#' data_collection_program(interactive = FALSE)
+data_collection_program <- function(interactive = TRUE) {
+
+  if (all(!interactive(), interactive)) {
+    message(
+      paste("Cannot run data_collection_program interactively.",
+        "Setting interactive = FALSE."))
+    interactive <- FALSE
+  }
+
+  observation <- if (interactive) {
+    data_collection_program_interactive()
+  } else {
+    data_collection_program_manual()
+  }
+
+  return(observation)
+}
